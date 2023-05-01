fbd1aded4316b52e5cfb7cd397cb975bb2e6992f
santosleon <leonsantos.dev@gmail.com>
2023-04-30 21:55:29 -0300

    wikipedia dataset

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 #define MIN_POINTS 2        // - The minimum number of data points required to estimate the model parameters.
 #define MAX_ITERATIONS 100  // - The maximum number of iterations allowed in the algorithm.
 #define C 0.00              // - The percent of close data points (inliers) required to assert that the model fits well to the data.
-#define E 0.10              // - A threshold value to determine data points that are fit well by the model (inlier).
+#define E 0.05              // - A threshold value to determine data points that are fit well by the model (inlier).
 
 typedef struct {
   float x;
@@ -14,7 +14,7 @@ typedef struct {
 } Point;
 
 #define POINT_SIZE sizeof(Point)*2
-#define MAX_POINTS 500
+#define MAX_POINTS 69
 
 #define SIZEOF_MEM ((MAX_POINTS * 2) + MIN_POINTS) * POINT_SIZE
 
@@ -31,7 +31,7 @@ float coefficientOfDetermination(Point* data, int n, float model[2], float* avg_
   for (int i = 0; i < n; i++) {
     float f = model[0] * data[i].x + model[1];
     float y_f = (data[i].y - f);
-    float y_y = (data[i].y - *(avg_y));
+    float y_y = (data[i].y - (*avg_y));
     ss_res += y_f * y_f;
     ss_tot += y_y * y_y;
   }
@@ -58,18 +58,12 @@ void leastSquare(Point* data, int n, float model[2], float* avg_y) {
   model[1] = b;
 }
 
-float distToLine(Point point, float model[2], float square) {
-  return fabs(model[0] * point.x - point.y + model[1]) / square;
-}
-
-
 void checkModel(Point* data, Point* temp, int data_size, int temp_size) {
-  float model[2];
-  
-  float *avg_y;
-  leastSquare(temp, temp_size, model, avg_y);
-  
   clock_t t;
+  float model[2];
+  float avg_y;
+  leastSquare(temp, temp_size, model, &avg_y);
+  
 
   Point inliers[MAX_POINTS];
   int inlinersQty = 0;
@@ -88,13 +82,14 @@ void checkModel(Point* data, Point* temp, int data_size, int temp_size) {
   if (inlinersQty >= 2 && inlinersQty >= (int)(data_size * C)) {
     float inliersModel[2];
     float fit;
+
     t = clock();
-    leastSquare(inliers, inlinersQty, inliersModel, avg_y);
+    leastSquare(inliers, inlinersQty, inliersModel, &avg_y);
     t = clock() - t;
     totalLeastSquareTime += ((float)t);
     
     t = clock();
-    fit = coefficientOfDetermination(data, data_size, inliersModel, avg_y);
+    fit = coefficientOfDetermination(data, data_size, inliersModel, &avg_y);
     t = clock() - t;
     totalFitTime += ((float)t);
 
@@ -128,13 +123,50 @@ void RANSAC(Point* data, int data_size) {
 }
 
 int main() {
+
+  float dataX[] = {
+    -0.848,-0.800,-0.704,-0.632,-0.488,
+    -0.472,-0.368,-0.336,-0.280,-0.200,
+    -0.00800,-0.0840,0.0240,0.100,0.124,
+    0.148,0.232,0.236,0.324,0.356,0.368,
+    0.440,0.512,0.548,0.660,0.640,0.712,
+    0.752,0.776,0.880,0.920,0.944,-0.108,
+    -0.168,-0.720,-0.784,-0.224,-0.604,
+    -0.740,-0.0440,0.388,-0.0200,0.752,
+    0.416,-0.0800,-0.348,0.988,0.776,
+    0.680,0.880,-0.816,-0.424,-0.932,
+    0.272,-0.556,-0.568,-0.600,-0.716,
+    -0.796,-0.880,-0.972,-0.916,0.816,
+    0.892,0.956,0.980,0.988,0.992,
+    0.004
+  };
+
+  float dataY[] = {
+    -0.917,-0.833,-0.801,-0.665,-0.605,
+    -0.545,-0.509,-0.433,-0.397,-0.281,
+    -0.205,-0.169,-0.0531,-0.0651,0.0349,
+    0.0829,0.0589,0.175,0.179,0.191,0.259,
+    0.287,0.359,0.395,0.483,0.539,0.543,
+    0.603,0.667,0.679,0.751,0.803,-0.265,
+    -0.341,0.111,-0.113,0.547,0.791,0.551,
+    0.347,0.975,0.943,-0.249,-0.769,-0.625,
+    -0.861,-0.749,-0.945,-0.493,0.163,
+    -0.469,0.0669,0.891,0.623,-0.609,
+    -0.677,-0.721,-0.745,-0.885,-0.897,
+    -0.969,-0.949,0.707,0.783,0.859,
+    0.979,0.811,0.891,-0.137
+  };
+
+
   unsigned int seed = 42;
   srand(seed); /* Intializes random number generator */
   Point data[MAX_POINTS];
   Point newPoint;
   for (int i = 0; i < MAX_POINTS; i++) {
-    newPoint.x = rand() / ((float)RAND_MAX);
-    newPoint.y = rand() / ((float)RAND_MAX);
+    newPoint.x = dataX[i];
+    newPoint.y = dataY[i];
+    // newPoint.x = rand() / ((float)RAND_MAX);
+    // newPoint.y = rand() / ((float)RAND_MAX);
     data[i] = newPoint;
   }
 
