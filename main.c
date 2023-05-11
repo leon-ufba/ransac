f6bd97a0c2dcf9dac48f630e50b721f8a5a89060
leon-ufba <80073744+leon-ufba@users.noreply.github.com>
2023-05-11 18:22:34 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,152 +1,171 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-// #include <time.h>
+#include <time.h>
 
-#define P 2    // - The minimum number of data points required to estimate the model parameters.
-#define T 100  // - The maximum number of iterations allowed in the algorithm.
-#define C 0.10 // - The percent of close data points (inliers) required to assert that the model fits well to the data.
-#define E 0.10 // - A threshold value to determine data points that are fit well by the model (inlier).
+typedef struct {
+  float x;
+  float y;
+} Point;
 
-volatile double bestModel[2] = {0, 0};
-volatile double bestFit = INFINITY;
+typedef struct {
+  float a;
+  float b;
+} Line;
+
+#define MIN_POINTS 2        // - The minimum number of data points required to estimate the model parameters.
+#define C 0.00              // - The percent of close data points (inliers) required to assert that the model fits well to the data.
+#define E 0.10              // - A threshold value to determine data points that are fit well by the model (inlier).
+
+#define POINT_SIZE sizeof(Point)*2
+#define MAX_POINTS 500
+
+#define SIZEOF_MEM ((MAX_POINTS * 2) + MIN_POINTS) * POINT_SIZE
+
+volatile Line bestModel = { 0.0, 0.0 };
+volatile float bestFit = INFINITY;
 volatile int verbose = 0;
-// volatile double totalTime = 0;
+volatile float totalDistToPointTime = 0;
+volatile float totalLeastSquareTime = 0;
+volatile float totalFitTime = 0;
 
-double** malloc2d(int n) {
-  double** arr = malloc(n * sizeof(double*));
+float coefficientOfDetermination(Point* data, int n, Line model, float avg_y) {
+  float ss_res = 0;
+  float ss_tot = 0;
   for (int i = 0; i < n; i++) {
-    arr[i] = malloc(2 * sizeof(double));
-  }
-  return arr;
-}
-
-void free2d(double** arr, int n) {
-  for (int i = 0; i < n; i++) {
-    free(arr[i]);
-  }
-  free(arr);
-}
-
-double coefficientOfDetermination(double** data, int n, double model[2]) {
-  double y_avg = 0;
-  double ss_res = 0;
-  double ss_tot = 0;
-  for (int i = 0; i < n; i++) {
-    y_avg += data[i][1];
-  }
-  y_avg /= n;
-  for (int i = 0; i < n; i++) {
-    double f = model[0] * data[i][0] + model[1];
-    double y_f = (data[i][1] - f);
-    double y_y = (data[i][1] - y_avg);
+    float f = model.a * data[i].x + model.b;
+    float y_f = (data[i].y - f);
+    float y_y = (data[i].y - avg_y);
     ss_res += y_f * y_f;
     ss_tot += y_y * y_y;
   }
-  // double r = 1 - (ss_res / ss_tot); // original coefficient
-  double r = (ss_res / ss_tot);	 // modified coefficient (lower is better)
+  // float r = 1 - (ss_res / ss_tot); // original coefficient
+  float r = (ss_res / ss_tot);  // modified coefficient (lower is better)
   return r;
 }
 
-void leastSquare(double** data, int n, double model[2]) {
-  double sx = 0;
-  double sy = 0;
-  double sxy = 0;
-  double sx2 = 0;
+void leastSquare(Point* data, int n, Line* model, float* avg_y) {
+  float sx = 0;
+  float sy = 0;
+  float sxy = 0;
+  float sx2 = 0;
   for (int i = 0; i < n; i++) {
-    sx += data[i][0];
-    sy += data[i][1];
-    sxy += data[i][0] * data[i][1];
-    sx2 += data[i][0] * data[i][0];
+    sx += data[i].x;
+    sy += data[i].y;
+    sxy += data[i].x * data[i].y;
+    sx2 += data[i].x * data[i].x;
   }
-  double a = (n * sxy - sx * sy) / (n * sx2 - sx * sx);
-  double b = (sy / n) - a * (sx / n);
-  model[0] = a;
-  model[1] = b;
+  (*model).a = (n * sxy - sx * sy) / (n * sx2 - sx * sx);
+  (*model).b = (sy / n) - (*model).a * (sx / n);
+  (*avg_y) = sy / n;
 }
 
-double distToLine(double* point, double model[2]) {
-  return fabs(model[0] * point[0] - point[1] + model[1]) / sqrt(model[0] * model[0] + 1.0);
-}
+void checkModel(Point* data, Point* temp, int data_size, int temp_size) {
+  clock_t t;
+  Line model;
+  float avg_y;
+  leastSquare(temp, temp_size, &model, &avg_y);
 
-void checkModel(double** data, double** temp, int data_size, int temp_size) {
-  double model[2];
-  leastSquare(temp, temp_size, model);
-
-
-  double** inliers = malloc2d(data_size);
+  Point inliers[MAX_POINTS];
   int inlinersQty = 0;
-
+  float square = E * sqrt(model.a * model.a + 1.0);
+  t = clock();
   for (int k = 0; k < data_size; k++) {
-    // clock_t t;
-    // t = clock();
-    double dist = distToLine(data[k], model);
-    // t = clock() - t;
-    // totalTime += ((double)t);
-    if (dist <= E) {
-      inliers[inlinersQty][0] = data[k][0];
-      inliers[inlinersQty][1] = data[k][1];
+    float dist = (model.a * data[k].x - data[k].y + model.b);
+    if (dist <= square) {
+      inliers[inlinersQty] = data[k];
       inlinersQty++;
     }
   }
+  t = clock() - t;
+  totalDistToPointTime += ((float)t);
 
   if (inlinersQty >= 2 && inlinersQty >= (int)(data_size * C)) {
-    double inliersModel[2];
-    double fit;
-    leastSquare(inliers, inlinersQty, inliersModel);
-    fit = coefficientOfDetermination(data, data_size, inliersModel);
-    if (verbose) {
-      for (int o = 0; o < temp_size; o++) printf("{%lf, %lf}  ", temp[o][0], temp[o][1]);
-      printf("\n");
-      printf("%d\n", inlinersQty);
-      printf("%lf\n", fit);
-      printf("%lf  %lf\n", model[0], model[1]);
-      printf("\n");
-    }
+    Line inliersModel;
+    float fit;
+
+    t = clock();
+    leastSquare(inliers, inlinersQty, &inliersModel, &avg_y);
+    t = clock() - t;
+    totalLeastSquareTime += ((float)t);
+    
+    t = clock();
+    fit = coefficientOfDetermination(data, data_size, inliersModel, avg_y);
+    t = clock() - t;
+    totalFitTime += ((float)t);
+
     if (fit < bestFit) {
       bestFit = fit;
-      bestModel[0] = inliersModel[0];
-      bestModel[1] = inliersModel[1];
+      bestModel = inliersModel;
     }
   }
 
-  free2d(inliers, inlinersQty);
-
 }
 
-void RANSAC(double** data, int data_size) {
-  int temp_size = P;
-  double** temp = malloc2d(temp_size);
+void RANSAC(Point* data, int data_size) {
+  int temp_size = MIN_POINTS;
+  Point temp[2]; // alocação tamanho P
   for (int i = 0; i < data_size; i++) {
-    temp[0][0] = data[i][0];
-    temp[0][1] = data[i][1];
+    temp[0] = data[i];
     for (int j = i + 1; j < data_size; j++) {
-      temp[1][0] = data[j][0];
-      temp[1][1] = data[j][1];
+      temp[1] = data[j];
       checkModel(data, temp, data_size, temp_size);
     }
   }
-  free2d(temp, temp_size);
 }
 
 int main() {
-  double data_arr[][2] = { {0, 1}, {1, 2}, {2, 4}, {3, 4}, {4, 5} };
-  // double data[][2] = {{95,90}, {85,80}, {80,70}, {70,65}, {60,60}};
-  int data_size = sizeof(data_arr) / sizeof(data_arr[0]);	 // number of data points
-  double** data = malloc2d(data_size);
-  for (int i = 0; i < data_size; i++) {
-    data[i][0] = data_arr[i][0];
-    data[i][1] = data_arr[i][1];
-  }
-  RANSAC(data, data_size);
-  free2d(data, data_size);
+  unsigned int seed = 42;
+  srand(seed);
+  Point data[MAX_POINTS];
+  Point newPoint;
 
-  // printf("\n------------------------\n");
-  // printf("totalTime - %lf\n", totalTime/CLOCKS_PER_SEC);
+  float dataX[] = {
+    -0.848, -0.800, -0.704, -0.632, -0.488, -0.472, -0.368, -0.336, -0.280, -0.200,
+    -0.008, -0.084,  0.024,  0.100,  0.124,  0.148,  0.232,  0.236,  0.324,  0.356,
+     0.368,  0.440,  0.512,  0.548,  0.660,  0.640,  0.712,  0.752,  0.776,  0.880,
+     0.920,  0.944, -0.108, -0.168, -0.720, -0.784, -0.224, -0.604, -0.740, -0.044,
+     0.388, -0.020,  0.752,  0.416, -0.080, -0.348,  0.988,  0.776,  0.680,  0.880,
+    -0.816, -0.424, -0.932,  0.272, -0.556, -0.568, -0.600, -0.716, -0.796, -0.880,
+    -0.972, -0.916,  0.816,  0.892,  0.956,  0.980,  0.988,  0.992,  0.004,  0.111
+  };
+
+  float dataY[] = {
+    -0.917, -0.833, -0.801, -0.665, -0.605, -0.545, -0.509, -0.433, -0.397, -0.281,
+    -0.205, -0.169, -0.053, -0.065,  0.035,  0.083,  0.059,  0.175,  0.179,  0.191,
+     0.259,  0.287,  0.359,  0.395,  0.483,  0.539,  0.543,  0.603,  0.667,  0.679,
+     0.751,  0.803, -0.265, -0.341,  0.111, -0.113,  0.547,  0.791,  0.551,  0.347,
+     0.975,  0.943, -0.249, -0.769, -0.625, -0.861, -0.749, -0.945, -0.493,  0.163,
+    -0.469,  0.067,  0.891,  0.623, -0.609, -0.677, -0.721, -0.745, -0.885, -0.897,
+    -0.969, -0.949,  0.707,  0.783,  0.859,  0.979,  0.811,  0.891, -0.137,  0.111
+  };
+
+  int data_size = sizeof(dataX) / sizeof(float); // number of data points
+  if(data_size > MAX_POINTS) data_size = MAX_POINTS;
+
+  for (int i = 0; i < data_size; i++) {
+    newPoint.x = dataX[i];
+    newPoint.y = dataY[i];
+    // newPoint.x = rand() / ((float)RAND_MAX);
+    // newPoint.y = rand() / ((float)RAND_MAX);
+    data[i] = newPoint;
+  }
+
+  clock_t t;
+  t = clock();
+  RANSAC(data, data_size);
+  t = clock() - t;
+  float totalTime = ((float)t);
+
+  printf("\n------------------------\n");
+  printf("totalDistToPointTime - %f\t%f\n", totalDistToPointTime / CLOCKS_PER_SEC, totalDistToPointTime / totalTime);
+  printf("totalLeastSquareTime - %f\t%f\n", totalLeastSquareTime / CLOCKS_PER_SEC, totalLeastSquareTime / totalTime);
+  printf("totalFitTime         - %f\t%f\n", totalFitTime         / CLOCKS_PER_SEC, totalFitTime         / totalTime);
+  printf("totalTime            - %f\t%f\n", totalTime            / CLOCKS_PER_SEC, totalTime            / totalTime);
   printf("\n------------------------\n");
   printf("\n");
-  printf("bestFit:  \t%lf\n", bestFit);
-  printf("bestModel:\t%lf\t%lf\n", bestModel[0], bestModel[1]);
+  printf("bestFit:  \t%f\n", bestFit);
+  printf("bestModel:\t%f\t%f\n", bestModel.a, bestModel.b);
   printf("\n");
 
   return 0;
