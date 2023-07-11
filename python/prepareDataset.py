0f4c0f243e95acac1adeacb3ae165c4a27c39b67
Daniel Carneiro <daniel.rcarneiro@yahoo.com>
2023-07-11 19:17:37 -0300

    added simplify dataset function

--- python/prepareDataset.py
+++ python/prepareDataset.py
@@ -2,9 +2,34 @@ from PIL import Image
 import os
 import numpy as np
 import pickle
+import math
 
 dirname = os.path.dirname(__file__)
 Image.MAX_IMAGE_PIXELS = None
+
+def calculate_distance(point1, point2):
+    x1, y1 = point1
+    x2, y2 = point2
+    return math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+
+def reduce_coordinates(input_file, output_file, radius):
+    with open(input_file, 'rb') as file:
+        coordinates = pickle.load(file)
+
+    simplified_coordinates = [coordinates[0]]  # Start with the first coordinate
+
+    for coord in coordinates[1:]:
+        distance = np.linalg.norm(coord - simplified_coordinates[-1])
+
+        if distance >= radius:
+            simplified_coordinates.append(coord)
+    
+    simplified_coordinates = np.array(simplified_coordinates)
+
+    with open(output_file, 'wb') as file:
+        pickle.dump(simplified_coordinates, file)
+
+    print(f"Coordenadas do dataset reduzidas. Salvas no arquivo '{output_file}'.")
 
 def genCoords(dataset):
   dataset_path = 'imgsDatasets/' + dataset + '.bmp'
@@ -19,11 +44,18 @@ def genCoords(dataset):
   coords_path = 'coords/' + dataset + '.pickle'
   coords_path = os.path.join(dirname, coords_path)
 
+
   with open(coords_path, 'wb') as file:
     pickle.dump(coordinates, file)
 
 
 dataset = input("Digite o nome do dataset: ")
+simplify = input("Deseja simplificar o dataset? (Y/N)\n")
+if simplify == "Y":
+    simplify = True
+else:
+    simplify = False
+
 
 try:  
     image_path = 'imgsDatasets/' + dataset + '.bmp'
@@ -62,8 +94,13 @@ else:
     print("Convertido o dataset " + dataset + " para cores binarias e exportado para o arquivo " + output + ".")
 
 try:
-    genCoords(dataset)   
+    genCoords(dataset)  
 except:
      print("Erro ao gerar coordenadas para o dataset.")
 else:
-    print("Coordenadas geradas para o dataset.")
+    print("Coordenadas geradas com sucesso para o dataset.")
+    
+    if simplify == True:
+        pickle_path = os.path.join(dirname, 'coords/' + dataset + '.pickle')
+        pickle_path2 = os.path.join(dirname, 'coords/' + dataset + 'SIMP.pickle')
+        reduce_coordinates(pickle_path, pickle_path2, 10.0) 
