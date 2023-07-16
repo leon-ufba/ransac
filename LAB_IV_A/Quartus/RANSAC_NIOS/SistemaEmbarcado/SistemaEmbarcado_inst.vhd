1a78df06dd34de251c387ad782f0680e394e7789
Caroline Alves <sma.caroline@ymail.com>
2023-07-15 23:57:43 -0300

    Entrada e saída de dados txt via NIOS

---
+++ LAB_IV_A/Quartus/RANSAC_NIOS/SistemaEmbarcado/SistemaEmbarcado_inst.vhd
@@ -0,0 +1,13 @@
+	component SistemaEmbarcado is
+		port (
+			clk_clk       : in std_logic := 'X'; -- clk
+			reset_reset_n : in std_logic := 'X'  -- reset_n
+		);
+	end component SistemaEmbarcado;
+
+	u0 : component SistemaEmbarcado
+		port map (
+			clk_clk       => CONNECTED_TO_clk_clk,       --   clk.clk
+			reset_reset_n => CONNECTED_TO_reset_reset_n  -- reset.reset_n
+		);
+
